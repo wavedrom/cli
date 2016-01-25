014a08c4e4b278b9173429b7476f12b1aa26e462
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-01-24 22:08:24 -0800

    start improving travis script

--- install-phantomjs.sh
+++ install-phantomjs.sh
@@ -6,9 +6,9 @@ which unzip
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-macosx.zip
     unzip phantomjs-2.0.0-macosx.zipphantomjs-2.0.0-macosx.zip
-    ln -s phantomjs-2.1.1-linux-x86_64 phantomjs
+    ln -s phantomjs-2.1.1-macosx phantomjs
 else
     wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
     tar -xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
-    ln -s phantomjs-2.1.1-macosx phantomjs
+    ln -s phantomjs-2.1.1-linux-x86_64 phantomjs
 fi
