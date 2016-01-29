7a2e92d49280e812a167cda5aac3456c0754bfff
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-01-29 08:43:42 -0800

    added more examples

---
+++ test/src/step7.js
@@ -0,0 +1,7 @@
+{ signal: [
+  { name: "clk",     wave: "p....." },
+  { name: "Data",    wave: "x345.x",  data: ["head", "center", "tail"] },
+  { name: "Request", wave: "01...0" }
+  ],
+  config: { hscale: 1 }
+}
